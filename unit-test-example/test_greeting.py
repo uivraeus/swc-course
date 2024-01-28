4949f639698c28fb18298b81e24e16d6bbcecfd3
Ulf Ivraeus <ulf.ivraeus@combitech.com>
2024-01-28 06:12:12 +0000

    Refine test examples

--- unit-test-example/test_greeting.py
+++ unit-test-example/test_greeting.py
@@ -6,20 +6,23 @@ import pytest
 
 from greeting import Greeter
 
-
-def test_ulf():
-    "Test with phrase override"
-    greeter = Greeter("Yo!")
-
-    response = greeter.hello("Ulf", 47)
-    assert "Yo! Ulf. You're on fire!" == response
-
-def test_junior():
+def test_default():
     "Test with default phrase"
     greeter = Greeter()
 
     response = greeter.hello("Junior", 24)
     assert "Hello Junior. You're young and very promising!" == response
 
-if __name__ == '__main__':
-    pytest.main()
+def test_override():
+    "Test with phrase override"
+    greeter = Greeter("Yo!")
+
+    response = greeter.hello("Ulf", 47)
+    assert "Yo! Ulf. You're on fire!" == response
+
+def test_invalid():
+    "Test negative age"
+    greeter = Greeter()
+
+    response = greeter.hello("Negative", -1)
+    assert "Hello Negative. What a strange age!" == response
